db5902be9aff009ee17a5df53ded765acee216ab
leillaury <75170725@certus.edu.pe>
2023-10-27 01:38:55 -0500

    disfracez parte lei

--- subida disfracez.mongodb.js
+++ subida disfracez.mongodb.js
@@ -1,2 +1,58 @@
 use("tienda")
-db.getCollection("Disfracez").insertMany()
+db.getCollection("Disfracez").insertMany([
+
+   {
+      "precio": 289 ,
+      "id": 1,
+      "title": "DISFRAZ DE LA VIUDA NEGRA",
+      "tema": "Superhéroes",
+      "genero": "Mujer",
+      "talla": "XS",
+      "thumbnailUrl": "https://static1.funidelia.com/518827-f6_big2/disfraz-de-la-viuda-negra-para-mujer.jpg" 
+   },
+   {
+      "precio": 268,
+      "id": 2,
+      "title": "DISFRAZ DE POWER RANGER NEGRO",
+      "tema": "Superhéroes",
+      "genero": "Hombre",
+      "talla": "XL",
+      "thumbnailUrl": "https://static1.funidelia.com/24630-f6_big2/disfraz-de-power-ranger-negro-morphsuit.jpg" 
+   },
+   {
+      "precio": 224,
+      "id": 3,
+      "title": "DISFRAZ DE CAPITANA MARVEL",
+      "tema": "Superhéroes",
+      "genero": "Mujer",
+      "talla": "XS",
+      "thumbnailUrl": "https://static1.funidelia.com/190329-f6_big2/disfraz-de-capitana-marvel-para-mujer.jpg" 
+   },
+   {
+      "precio": 119,
+      "id": 4,
+      "title": "DISFRAZ DE CONDE DRACULA",
+      "tema": "Vampiros",
+      "genero": "Hombre",
+      "talla": "M",
+      "thumbnailUrl": "https://static1.funidelia.com/518495-f6_big2/disfraz-de-conde-dracula-para-hombre.jpg" 
+   },
+   {
+      "precio": 120,
+      "id": 5,
+      "title": "DISFRAZ DE PITUFO",
+      "tema": "Dibujos Animados",
+      "genero": "Hombre",
+      "talla": "XL",
+      "thumbnailUrl": "https://static1.funidelia.com/105181-f6_big2/disfraz-de-pitufo.jpg" 
+   },
+   {
+      "precio": 110,
+      "id": 6,
+      "title": "DISFRAZ DE SPIDERGIRL",
+      "tema": "Superhéroes",
+      "genero": "Mujer",
+      "talla": "S",
+      "thumbnailUrl": "https://static1.funidelia.com/40597-f6_big2/disfraz-de-spidergirl-para-mujer.jpg" 
+   },
+])
