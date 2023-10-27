f2c99cebf719b7cc7b5c71c76e4d2646105490af
Nicolesmc <nicolsharon.04@gmail.com>
2023-10-27 10:05:02 -0500

    mi parte del trabajo

---
+++ playground-1.mongodb.js
@@ -0,0 +1,115 @@
+use("tienda")
+
+db.getCollection("calzado").insertMany([
+
+    {
+        "precio": 180,
+        "id": 1,
+        "title": "Jordan",
+        "marca": "Nike",
+        "color": "Verde Pastel",
+        "talla": 36,
+        "thumbnailUrl": "https://i.pinimg.com/564x/34/5c/24/345c24589e37535038505751b7f3826e.jpg" 
+     },
+     {
+        "precio": 200,
+        "id": 2,
+        "title": "Clasics",
+        "marca": "Converse",
+        "color": "Negro",
+        "talla": 38,
+        "thumbnailUrl": "https://i.pinimg.com/564x/2b/5b/f1/2b5bf15f71448cc1adf6f9e2a9f4fb21.jpg"
+    },
+     {
+        "precio": 180,
+        "id": 3,
+        "title": "Superstrar",
+        "marca": "Adidas",
+        "color": "Blanco",
+        "talla": 37,
+        "thumbnailUrl": "https://i.pinimg.com/564x/7a/a7/5c/7aa75c93056b4b157e78062070f1bcb6.jpg"    
+     },
+     {
+        "precio": 190,
+        "id": 4,
+        "title": "Jordan",
+        "marca": "Nike",
+        "color": "Plomo y Blanco",
+        "talla": 36,
+        "thumbnailUrl": "https://i.pinimg.com/564x/9b/5c/70/9b5c704a69dd74751bcbbe0f006e59fe.jpg"   
+     },
+     {  
+        "precio": 180,
+        "id": 5,
+        "title": "Clasics",
+        "marca": "Converse",
+        "color": "Rosado Pastel",
+        "talla": 35,
+        "thumbnailUrl": "https://i.pinimg.com/564x/47/51/a7/4751a70bc895fc4aa33357f8ac9efbbf.jpg" 
+     },
+     {
+        "precio": 1420,
+        "id": 6,
+        "title": "Botines",
+        "marca": "Prada",
+        "color": "Blanco",
+        "talla": 37,
+        "thumbnailUrl": "https://i.pinimg.com/564x/7c/20/36/7c203607dda6efe73fa14820f35d2f27.jpg"     
+     },
+     {
+        "precio": 200,
+        "id": 7,
+        "title": "Baskets plates",
+        "marca": "Fengguang",
+        "color": "Blanco",
+        "talla": 39,
+        "thumbnailUrl": "https://i.pinimg.com/564x/48/88/98/488898f703d29b1eba8871141f5f0926.jpg"     
+     },
+     {
+        "precio": 210,
+        "id": 8,
+        "title": "Jordan",
+        "marca": "Nike",
+        "color": "Negro",
+        "talla": 39,
+        "thumbnailUrl": "https://i.pinimg.com/564x/4d/1f/91/4d1f91b63106cc22019af700b9ed127f.jpg"     
+     },
+     {
+        "precio": 180,
+        "id": 9,
+        "title": "Tommy men",
+        "marca": "Tommy Hilfiger",
+        "color": "Blancas",
+        "talla": 40,
+        "thumbnailUrl": "https://i.pinimg.com/564x/a6/f8/05/a6f805105394e621763de962216c5dd7.jpg"     
+     },
+     {
+        "precio": 220,
+        "id": 10,
+        "title": "Nike Air",
+        "marca": "Nike",
+        "color": "Azul claro y Blanco ",
+        "talla": 39,
+        "thumbnailUrl": "https://i.pinimg.com/564x/49/9a/f3/499af3b76a5625c85f25330c36d6ac2a.jpg"     
+     },
+     {
+        "precio": 230,
+        "id": 11,
+        "title": "Black Sneakers",
+        "marca": "Amiri",
+        "color": "Negro",
+        "talla": 40,
+        "thumbnailUrl": "https://i.pinimg.com/564x/5c/9c/d6/5c9cd6de1de640c18921a0c9b27108ce.jpg"     
+     },
+     {
+        "precio": 250,
+        "id": 1,
+        "title": "Street Style Sneaker",
+        "marca": "Louis Vuitton",
+        "color": "Gris",
+        "talla": 40,
+        "thumbnailUrl": "https://i.pinimg.com/564x/b8/36/b2/b836b2749ace86cb8ed656dd1109c21a.jpg"     
+     }
+
+])
+
